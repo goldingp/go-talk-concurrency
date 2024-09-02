2adf6f471ca5e37ebfc8ab3d0bb1e1270cd96ac7
goldingp <6238669+goldingp@users.noreply.github.com>
2024-09-02 00:24:48 -0600

    Remove a close channel operation from a reception go routine

--- internal/multiplex/fan_in.go
+++ internal/multiplex/fan_in.go
@@ -1,22 +1,22 @@
 package multiplex
 
+import "sync"
+
 func FanIn[T any](in ...<-chan T) <-chan T {
 	c := make(chan T)
-	closed := make(chan bool, len(in))
+	wg := &sync.WaitGroup{}
 	for i := range in {
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			for obj := range in[i] {
 				c <- obj
 			}
-			closed <- true
 		}()
 	}
 	go func() {
-		defer close(closed)
 		defer close(c)
-		for i := 0; i < len(in); i++ {
-			<-closed
-		}
+		wg.Wait()
 	}()
 	return c
 }
